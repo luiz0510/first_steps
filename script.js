b291f8cd81de92b05539197c261866dc09a8a536
Luiz Eduardo <luiz@atlasgov.com>
2020-10-13 23:09:27 -0300

    agora vai

---
+++ script.js
@@ -0,0 +1,9 @@
+//var nome = "Luiz";
+//funçao entrar
+function entrar(){
+    
+    var area = document.getElementById('area');
+    var texto = prompt('Digite seu nome?');
+
+    area.innerHTML = texto;
+}
